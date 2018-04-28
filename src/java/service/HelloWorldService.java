e88c864efe35a76cec23ec30fc5e1676f95aa256
Tomasz Suchwałko <tomasz.suchwalko@gmail.com>
2018-04-28 12:30:27 +0200

    2 done

---
+++ src/java/service/HelloWorldService.java
@@ -0,0 +1,29 @@
+package service;
+
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+
+@Path("hello")
+public class HelloWorldService {
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getHtml(){
+        return "Witaj JAX-RS";
+    }
+    
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getHtml3(){
+        return "Witaj JAX-RS3";
+    }
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("echo")
+    public String getHtml2(){
+        return "Witaj JAX-RS2";
+    }
+}
